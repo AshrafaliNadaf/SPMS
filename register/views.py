c82e90c76a76511092bed60c994a71d5f8744345
unknown <cngsharath@gmail.com>
2020-09-23 09:43:56 +0530

    login works

--- register/views.py
+++ register/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.shortcuts import render,redirect,reverse
+from django.contrib import auth
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 
@@ -8,4 +11,22 @@ from django.shortcuts import render
 
 
 def login(request):
-    return render(request, 'LoginPage.html')
+    if request.method == "POST":
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            if user.last_name == "admin":
+                auth.login(request, user)
+                return HttpResponse( 'Logged in ADMIN')
+            elif user.last_name == "staff":
+                auth.login(request, user)
+                return HttpResponse( 'Logged in STAFF')
+            elif user.last_name == "faculty":
+                auth.login(request, user)
+                return HttpResponse( 'Logged in FACULTY')
+            else :
+                auth.login(request, user)
+                return HttpResponse( 'Logged in STUDENT')
+        else:
+            return HttpResponse( 'usernameError')
+    else:
+        return render(request, 'LoginPage.html')
